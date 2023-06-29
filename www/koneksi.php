a25dec860039d7ada13b48393a88fc027455d683
adhemukhlis <adhemukhlis@gmail.com>
2023-06-30 00:44:02 +0700

    update koneksi for local connection

--- www/koneksi.php
+++ www/koneksi.php
@@ -2,5 +2,9 @@
 if (!isset($_SESSION)) {
     session_start();
 }
+// local db connection
+// $koneksi = new mysqli("localhost", "root", "", "untukkatakog");
+
+// docker db connection
 $koneksi = new mysqli("db", "user", "test", "untukkatakog");
 
