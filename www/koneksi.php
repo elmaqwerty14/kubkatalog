eae4eadb1bfd0eb7c3f22dc9068355cdd98147df
elmaqwerty14 <elmakiu333@gmail.com>
2023-06-29 21:34:14 +0700

    ubah koneksi php

--- www/koneksi.php
+++ www/koneksi.php
@@ -2,4 +2,4 @@
 if (!isset($_SESSION)) {
     session_start();
 }
-$koneksi = new mysqli("localhost", "root", "", "untukkatakog");
+$koneksi = new mysqli("localhost:2022", "root", "", "untukkatakog");
